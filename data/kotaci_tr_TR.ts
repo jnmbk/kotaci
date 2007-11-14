fb5a70bf2adaf08773cf031c1e7a44826e31795f
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-11-14 12:56:38 +0000

    update translation

--- data/kotaci_tr_TR.ts
+++ data/kotaci_tr_TR.ts
@@ -1,22 +1,22 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="tr">
 <defaultcodec></defaultcodec>
-<context encoding="UTF-8">
+<context>
     <name>CaptchaDialog</name>
-    <message encoding="UTF-8">
+    <message>
         <location filename="captchawindow.py" line="52"/>
-        <source>Kotacı</source>
-        <translation type="unfinished"></translation>
+        <source>Kotaci</source>
+        <translation>Kotacı</translation>
     </message>
-    <message encoding="UTF-8">
+    <message>
         <location filename="captchawindow.py" line="53"/>
-        <source>Aşağıdaki kutuya resimde gördüğünüzü yazın:</source>
-        <translation type="unfinished"></translation>
+        <source>Write what you see into the box below:</source>
+        <translation>Aşağıdaki kutuya resimde gördüğünüzü yazın:</translation>
     </message>
-    <message encoding="UTF-8">
+    <message>
         <location filename="captchawindow.py" line="54"/>
-        <source>Resmi Değiştir</source>
-        <translation type="unfinished"></translation>
+        <source>Change Picture</source>
+        <translation>Resmi Değiştir</translation>
     </message>
 </context>
 <context>
@@ -39,52 +39,125 @@
     <message>
         <location filename="configwindow.py" line="131"/>
         <source>Enter your TTnet username here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Buraya TTnet kullanıcı adınızı girin.</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="132"/>
         <source>Enter your TTnet password here.</source>
-        <translation type="unfinished"></translation>
+        <translation>Buraya TTnet parolanızı girin.</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="133"/>
         <source>Saves password to configuration file when selected. It may be unsafe to save the password.</source>
-        <translation type="unfinished"></translation>
+        <translation>Seçiliyse parolanız ayar dosyasına kaydedilir. Parolayı bu şekilde saklamak güvenli olmayabilir.</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="134"/>
         <source>Save password</source>
-        <translation type="unfinished"></translation>
+        <translation>Parolayı kaydet</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="135"/>
         <source>Account</source>
-        <translation type="unfinished"></translation>
+        <translation>Hesap</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="136"/>
         <source>Text color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Yazı rengi:</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="137"/>
         <source>Background color:</source>
-        <translation type="unfinished"></translation>
+        <translation>Arka plan rengi:</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="138"/>
         <source>Select tray icon text color.</source>
-        <translation type="unfinished"></translation>
+        <translation>Simgenin yazı rengini seçin.</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="139"/>
         <source>Select tray icon background color.</source>
-        <translation type="unfinished"></translation>
+        <translation>Simgenin arka plan rengini seçin.</translation>
     </message>
     <message>
         <location filename="configwindow.py" line="140"/>
         <source>Tray Icon</source>
-        <translation type="unfinished"></translation>
+        <translation>Sistem Çekmecesi</translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="kotaci.py" line="108"/>
+        <source>Used Qouta</source>
+        <translation>Kullanılan Kota</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="110"/>
+        <source>Latest Update:</source>
+        <translation>Son Güncelleme:</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="113"/>
+        <source>Please right click and check quata.</source>
+        <translation>Kotayı kontrol etmek için çift tıklayın.</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="119"/>
+        <source>Loading, plase wait...</source>
+        <translation>Yükleniyor, bekleyin...</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="132"/>
+        <source>Error</source>
+        <translation>Hata</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="130"/>
+        <source>System Error</source>
+        <translation>Sistem Hatası</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="134"/>
+        <source>Login error. Be sure you wrote it corrcetly and have specified a username in configuration.</source>
+        <translation>Giriş hatası. Yazıyı doğru yazdığınızdan ve ayarlarınızın doğru olduğundan emin olun.</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="144"/>
+        <source>Quota Information</source>
+        <translation>Kota Bilgisi</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="166"/>
+        <source>About Kotaci</source>
+        <translation>Kotacı Hakkında</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="171"/>
+        <source>&lt;b&gt;Kotaci %1&lt;/b&gt; - ttnet ADSL quota displayer&lt;br /&gt;Copyright (c) 2007, Ugur Cetin &lt;ugur.jnmbk at gmail.com&gt;&lt;br /&gt;This software is licensed under the terms of GPL-2.&lt;br /&gt;&lt;a href=&quot;http://kotaci.googlecode.com&quot;&gt;http://kotaci.googlecode.com&lt;/a&gt;</source>
+        <translation>&lt;b&gt;Kotaci %1&lt;/b&gt; - ttnet ADSL kota görüntüleyicisi&lt;br /&gt;Copyright (c) 2007, Uğur Çetin &lt;ugur.jnmbk at gmail.com&gt;&lt;br /&gt;Bu yazılım GPL-2 lisansıyla dağıtılmaktadır.&lt;br /&gt;&lt;a href=&quot;http://kotaci.googlecode.com&quot;&gt;http://kotaci.googlecode.com&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="192"/>
+        <source>About</source>
+        <translation>Hakkında</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="194"/>
+        <source>Check now...</source>
+        <translation>Şimdi kontrol et...</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="196"/>
+        <source>Configure...</source>
+        <translation>Yapılandır...</translation>
+    </message>
+    <message>
+        <location filename="kotaci.py" line="198"/>
+        <source>Exit</source>
+        <translation>Çık</translation>
     </message>
 </context>
 </TS>
