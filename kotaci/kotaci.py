4db75eba7165d7bc0c00f144fc24eeaf1df49dc0
Uğur Çetin <ugur@ugurcetin.com.tr>
2007-11-22 05:21:08 +0000

    use PyQt4 style importing, they recommend it...

--- kotaci/kotaci.py
+++ kotaci/kotaci.py
@@ -15,8 +15,8 @@
 
 import commands, httplib2, os, time, signal, sys, thread
 
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
+from PyQt4 import QtCore
+from PyQt4 import QtGui
 
 import configwindow, captchawindow, statswindow, kotaci_rc
 
@@ -26,14 +26,14 @@ def byte2gb(bytes, rounding=3):
 def getValues(results):
     months = [u'Ocak', u'\u015eubat', u'Mart', u'Nisan', u'May\u0131s', u'Haziran',
             u'Temmuz', u'A\u011fustos', u'Eyl\xfcl', u'Ekim', u'Kas\u0131m', u'Aral\u0131k']
-    date = QDate(int(results[0]), months.index(results[1])+1, 1)
+    date = QtCore.QDate(int(results[0]), months.index(results[1])+1, 1)
     upload = int(results[2].replace('.', ''))
     download = int(results[5].replace('.', ''))
     return (date, download, upload)
 
-class QuotaGrabber(QObject):
+class QuotaGrabber(QtCore.QObject):
     def __init__(self):
-        QObject.__init__(self)
+        QtCore.QObject.__init__(self)
         self.http = httplib2.Http()
 
     def getCatpcha(self):
@@ -44,10 +44,10 @@ class QuotaGrabber(QObject):
             }
         response, content = self.http.request(url, headers=request)
         self.cookie = {'Cookie': response['set-cookie']}
-        self.emit(SIGNAL("captchaWritten"), QByteArray(content))
+        self.emit(QtCore.SIGNAL("captchaWritten"), QtCore.QByteArray(content))
 
     def getResults(self, captcha, password=None, username=None):
-        settings = QSettings()
+        settings = QtCore.QSettings()
         if username is None:
             username = settings.value("username").toString()
         if password is None:
@@ -80,62 +80,62 @@ class QuotaGrabber(QObject):
             content = content.replace('<tr class="even">',"")
             content = content.replace('<td width="100">',"").replace('<br>&nbsp;'," ")
             content = content.replace('</tr>'," ").replace("</td>","")
-        self.emit(SIGNAL("gotResults"), content)
+        self.emit(QtCore.SIGNAL("gotResults"), content)
 
-class CaptchaWindow(QDialog, captchawindow.Ui_CaptchaDialog):
+class CaptchaWindow(QtGui.QDialog, captchawindow.Ui_CaptchaDialog):
     def __init__(self):
-        QDialog.__init__(self)
+        QtGui.QDialog.__init__(self)
         self.setupUi(self)
 
     def displayCaptcha(self, byteArray):
         self.captcha.clear()
-        pixmap = QPixmap()
+        pixmap = QtGui.QPixmap()
         pixmap.loadFromData(byteArray)
         self.captcha.setPixmap(pixmap)
 
 
-class StatsWindow(QDialog, statswindow.Ui_StatsWindow):
+class StatsWindow(QtGui.QDialog, statswindow.Ui_StatsWindow):
     def __init__(self):
-        QDialog.__init__(self)
+        QtGui.QDialog.__init__(self)
         self.setupUi(self)
-        QObject.connect(self.clearButton, SIGNAL("clicked()"), self.clearStats)
+        QtCore.QObject.connect(self.clearButton, QtCore.SIGNAL("clicked()"), self.clearStats)
         self.updateStats()
 
     def updateStats(self):
         self.stats.clear()
-        settings = QSettings()
+        settings = QtCore.QSettings()
         settings.beginGroup("Stats")
-        for key in [i for i in settings.childKeys() if QDate.fromString(i, "yyyyMM").isValid()]:
+        for key in [i for i in settings.childKeys() if QtCore.QDate.fromString(i, "yyyyMM").isValid()]:
             list = settings.value(key).toList()
-            item = QTreeWidgetItem(self.stats)
-            item.setText(0, QDate.fromString(key, "yyyyMM").toString("MMMM yyyy"))
-            item.setText(1, QString("%L2 GB").arg(byte2gb(list[0].toDouble()[0])))
-            item.setText(2, QString("%L2 GB").arg(byte2gb(list[1].toDouble()[0])))
+            item = QtGui.QTreeWidgetItem(self.stats)
+            item.setText(0, QtCore.QDate.fromString(key, "yyyyMM").toString("MMMM yyyy"))
+            item.setText(1, QtCore.QString("%L2 GB").arg(byte2gb(list[0].toDouble()[0])))
+            item.setText(2, QtCore.QString("%L2 GB").arg(byte2gb(list[1].toDouble()[0])))
         settings.endGroup()
-        item = QTreeWidgetItem(self.stats)
-        item.setText(0, QDate.currentDate().toString("MMMM yyyy"))
+        item = QtGui.QTreeWidgetItem(self.stats)
+        item.setText(0, QtCore.QDate.currentDate().toString("MMMM yyyy"))
         download = settings.value("lastReport/bytes").toDouble()[0]
-        item.setText(1, QString("%L2 GB").arg(byte2gb(download)))
+        item.setText(1, QtCore.QString("%L2 GB").arg(byte2gb(download)))
         upload = settings.value("lastReport/upload").toDouble()[0]
-        item.setText(2, QString("%L2 GB").arg(byte2gb(upload)))
+        item.setText(2, QtCore.QString("%L2 GB").arg(byte2gb(upload)))
 
     def clearStats(self):
-        settings = QSettings()
+        settings = QtCore.QSettings()
         settings.remove("Stats")
         self.updateStats()
 
-class TrayIcon(QSystemTrayIcon):
+class TrayIcon(QtGui.QSystemTrayIcon):
     def __init__(self):
-        QSystemTrayIcon.__init__(self)
+        QtGui.QSystemTrayIcon.__init__(self)
         self.refreshQuota()
         self.captchaWindow = CaptchaWindow()
         self.grabber = QuotaGrabber()
         self.statsWindow = StatsWindow()
-        QObject.connect(self.captchaWindow, SIGNAL("accepted()"), self.continueCheckQuota)
-        QObject.connect(self.captchaWindow.changePicture, SIGNAL("clicked()"), self.checkQuota)
-        QObject.connect(self.grabber, SIGNAL("captchaWritten"), self.captchaWindow.displayCaptcha)
-        QObject.connect(self.grabber, SIGNAL("gotResults"), self.continueCheckQuota)
-        QObject.connect(self, SIGNAL("activated(QSystemTrayIcon::ActivationReason)"), self.on_activated)
+        QtCore.QObject.connect(self.captchaWindow, QtCore.SIGNAL("accepted()"), self.continueCheckQuota)
+        QtCore.QObject.connect(self.captchaWindow.changePicture, QtCore.SIGNAL("clicked()"), self.checkQuota)
+        QtCore.QObject.connect(self.grabber, QtCore.SIGNAL("captchaWritten"), self.captchaWindow.displayCaptcha)
+        QtCore.QObject.connect(self.grabber, QtCore.SIGNAL("gotResults"), self.continueCheckQuota)
+        QtCore.QObject.connect(self, QtCore.SIGNAL("activated(QSystemTrayIcon::ActivationReason)"), self.on_activated)
 
     def on_activated(self, activationReason):
         # check quota on double click
@@ -144,19 +144,19 @@ class TrayIcon(QSystemTrayIcon):
 
     def refreshQuota(self):
         # reads quota information from settings, and changes tray icon accordingly
-        settings = QSettings()
+        settings = QtCore.QSettings()
         if settings.contains("lastReport/bytes"):
             quota = self.tr("%L1\nGB").arg(byte2gb(settings.value("lastReport/bytes").toDouble()[0], 2))
         else:
             quota = self.tr("?\nGB")
-        pixmap = QPixmap(32, 32)
-        pixmap.fill(QColor(settings.value("trayIcon/backgroundColor", QVariant("red")).toString()))
-        painter = QPainter(pixmap)
-        painter.setPen(QColor(settings.value("trayIcon/textColor", QVariant("white")).toString()))
-        painter.setFont(QFont("", 10, QFont.Bold))
-        painter.drawText(QRect(0, 0, 32, 32), Qt.AlignCenter, quota)
+        pixmap = QtGui.QPixmap(32, 32)
+        pixmap.fill(QtGui.QColor(settings.value("trayIcon/backgroundColor", QtCore.QVariant("red")).toString()))
+        painter = QtGui.QPainter(pixmap)
+        painter.setPen(QtGui.QColor(settings.value("trayIcon/textColor", QtCore.QVariant("white")).toString()))
+        painter.setFont(QtGui.QFont("", 10, QtGui.QFont.Bold))
+        painter.drawText(QtCore.QRect(0, 0, 32, 32), QtCore.Qt.AlignCenter, quota)
         painter.end()
-        icon = QIcon(pixmap)
+        icon = QtGui.QIcon(pixmap)
         self.setIcon(icon)
         if settings.contains("lastReport/bytes"):
             self.setToolTip(
@@ -179,9 +179,9 @@ class TrayIcon(QSystemTrayIcon):
             if self.captchaWindow.lineEdit.text() == "":
                 self.captchaWindow.show()
             else:
-                settings = QSettings()
-                if settings.value("savePassword").toInt()[0] != Qt.Checked:
-                    password = QInputDialog.getText(None, self.tr("Enter Password"), self.tr("Enter your TTnet password:"), QLineEdit.Password)[0]
+                settings = QtCore.QSettings()
+                if settings.value("savePassword").toInt()[0] != QtCore.Qt.Checked:
+                    password = QtGui.QInputDialog.getText(None, self.tr("Enter Password"), self.tr("Enter your TTnet password:"), QtGui.QLineEdit.Password)[0]
                     thread.start_new_thread(self.grabber.getResults, (self.captchaWindow.lineEdit.text(),password))
                 else:
                     thread.start_new_thread(self.grabber.getResults, (self.captchaWindow.lineEdit.text(),))
@@ -197,95 +197,95 @@ class TrayIcon(QSystemTrayIcon):
                 values = []
                 values.append(getValues(results.split()[:8]))
                 values.append(getValues(results.split()[8:16]))
-                settings = QSettings()
+                settings = QtCore.QSettings()
                 settings.beginGroup("Stats")
                 for value in values:
-                    settings.setValue(value[0].toString("yyyyMM"), QVariant([QVariant(value[1]), QVariant(value[2])]))
+                    settings.setValue(value[0].toString("yyyyMM"), QtCore.QVariant([QtCore.QVariant(value[1]), QtCore.QVariant(value[2])]))
                 settings.endGroup()
 
                 lastReport = results.split("\n")[-1]
                 lastReport = int(lastReport[:lastReport.index('(')-1].replace('.', ''))
-                settings.setValue("lastReport/bytes", QVariant(lastReport))
-                settings.setValue("lastReport/upload", QVariant(getValues(results.split()[16:])[2]))
-                settings.setValue("lastReport/date", QVariant(QDateTime.currentDateTime()))
+                settings.setValue("lastReport/bytes", QtCore.QVariant(lastReport))
+                settings.setValue("lastReport/upload", QtCore.QVariant(getValues(results.split()[16:])[2]))
+                settings.setValue("lastReport/date", QtCore.QVariant(QtCore.QDateTime.currentDateTime()))
                 self.refreshQuota()
                 self.showMessage(self.tr("Quota Information"), self.tr("%L1 bytes\n(%L2 GB)").arg(lastReport).arg(
                     byte2gb(float(lastReport))))
                 self.statsWindow.updateStats()
 
-class ConfigWindow(QDialog, configwindow.Ui_Dialog):
+class ConfigWindow(QtGui.QDialog, configwindow.Ui_Dialog):
     def __init__(self, trayIcon):
-        QDialog.__init__(self)
+        QtGui.QDialog.__init__(self)
         self.setupUi(self)
         self.trayIcon = trayIcon
-        QObject.connect(self, SIGNAL("accepted()"), self.saveSettings)
-        QObject.connect(self, SIGNAL("rejected()"), self.loadSettings)
+        QtCore.QObject.connect(self, QtCore.SIGNAL("accepted()"), self.saveSettings)
+        QtCore.QObject.connect(self, QtCore.SIGNAL("rejected()"), self.loadSettings)
         # for the color selector
-        for color in QColor.colorNames():
-            pixmap = QPixmap(16, 16)
-            pixmap.fill(QColor(color))
-            icon = QIcon(pixmap)
+        for color in QtGui.QColor.colorNames():
+            pixmap = QtGui.QPixmap(16, 16)
+            pixmap.fill(QtGui.QColor(color))
+            icon = QtGui.QIcon(pixmap)
             self.textColor.addItem(icon, color)
             self.backgroundColor.addItem(icon, color)
         self.loadSettings()
 
     def saveSettings(self):
-        settings = QSettings()
-        settings.setValue("username", QVariant(self.username.text()))
-        settings.setValue("password", QVariant(self.password.text()))
-        settings.setValue("savePassword", QVariant(self.savePassword.checkState()))
-        settings.setValue("trayIcon/textColor", QVariant(self.textColor.currentText()))
-        settings.setValue("trayIcon/backgroundColor", QVariant(self.backgroundColor.currentText()))
+        settings = QtCore.QSettings()
+        settings.setValue("username", QtCore.QVariant(self.username.text()))
+        settings.setValue("password", QtCore.QVariant(self.password.text()))
+        settings.setValue("savePassword", QtCore.QVariant(self.savePassword.checkState()))
+        settings.setValue("trayIcon/textColor", QtCore.QVariant(self.textColor.currentText()))
+        settings.setValue("trayIcon/backgroundColor", QtCore.QVariant(self.backgroundColor.currentText()))
         self.trayIcon.refreshQuota()
-        if self.savePassword.checkState() == Qt.Unchecked:
+        if self.savePassword.checkState() == QtCore.Qt.Unchecked:
             settings.remove("password")
 
     def loadSettings(self):
-        settings = QSettings()
+        settings = QtCore.QSettings()
         self.username.setText(settings.value("username").toString())
         self.password.setText(settings.value("password").toString())
-        self.savePassword.setCheckState(Qt.CheckState(settings.value("savePassword", QVariant(Qt.Checked)).toInt()[0]))
-        self.textColor.setCurrentIndex(self.textColor.findText(settings.value("trayIcon/textColor", QVariant("white")).toString()))
-        self.backgroundColor.setCurrentIndex(self.textColor.findText(settings.value("trayIcon/backgroundColor", QVariant("red")).toString()))
-        if self.savePassword.checkState() == Qt.Unchecked:
+        self.savePassword.setCheckState(QtCore.Qt.CheckState(settings.value("savePassword", QtCore.QVariant(QtCore.Qt.Checked)).toInt()[0]))
+        self.textColor.setCurrentIndex(self.textColor.findText(settings.value("trayIcon/textColor", QtCore.QVariant("white")).toString()))
+        self.backgroundColor.setCurrentIndex(self.textColor.findText(settings.value("trayIcon/backgroundColor", QtCore.QVariant("red")).toString()))
+        if self.savePassword.checkState() == QtCore.Qt.Unchecked:
             self.password.setEnabled(False)
 
 def about():
     import __init__
-    QMessageBox.about(None, QApplication.translate("TrayIcon", "About Kotaci", None, QApplication.UnicodeUTF8),
-        QApplication.translate("TrayIcon",
+    QtGui.QMessageBox.about(None, QtGui.QApplication.translate("TrayIcon", "About Kotaci", None, QtGui.QApplication.UnicodeUTF8),
+        QtGui.QApplication.translate("TrayIcon",
         "<b>Kotaci %1</b> - ttnet ADSL quota displayer<br />Copyright (c) 2007, Ugur Cetin <ugur.jnmbk at gmail.com><br />"\
         "This software is licensed under the terms of GPL-2.<br /><a href=\"http://kotaci.googlecode.com\">"\
         "http://kotaci.googlecode.com</a><br /><br />Kotaci nor its authors are in any way affiliated or endorsed by Turk Telekom.", None,
-        QApplication.UnicodeUTF8).arg(__init__.__version__))
+        QtGui.QApplication.UnicodeUTF8).arg(__init__.__version__))
 
 def main():
     signal.signal(signal.SIGINT, signal.SIG_DFL)
-    app = QApplication(sys.argv)
+    app = QtGui.QApplication(sys.argv)
     app.setApplicationName("kotaci")
     app.setOrganizationName("kotaci")
     app.setQuitOnLastWindowClosed(False)
-    settings = QSettings()
+    settings = QtCore.QSettings()
 
-    locale = QLocale.system().name()
-    translator = QTranslator()
+    locale = QtCore.QLocale.system().name()
+    translator = QtCore.QTranslator()
     translator.load(":/kotaci_%s.qm" % locale)
     app.installTranslator(translator)
 
     trayIcon = TrayIcon()
     configWindow = ConfigWindow(trayIcon)
 
-    menu = QMenu()
-    actionAbout = QAction(QIcon(":icons/help1.png"),
-        QApplication.translate("TrayIcon", "About", None, QApplication.UnicodeUTF8), menu)
-    actionCheckQuota = QAction(QIcon(":icons/ok.png"),
-        QApplication.translate("TrayIcon", "Check now...", None, QApplication.UnicodeUTF8), menu)
-    actionConfigure = QAction(QIcon(":icons/configure.png"),
-        QApplication.translate("TrayIcon", "Configure...", None, QApplication.UnicodeUTF8), menu)
-    actionStatistics = QAction(QIcon(":icons/today.png"),
-        QApplication.translate("TrayIcon", "Statistics...", None, QApplication.UnicodeUTF8), menu)
-    actionQuit = QAction(QIcon(":icons/exit.png"),
-        QApplication.translate("TrayIcon", "Exit", None, QApplication.UnicodeUTF8), menu)
+    menu = QtGui.QMenu()
+    actionAbout = QtGui.QAction(QtGui.QIcon(":icons/help1.png"),
+        QtGui.QApplication.translate("TrayIcon", "About", None, QtGui.QApplication.UnicodeUTF8), menu)
+    actionCheckQuota = QtGui.QAction(QtGui.QIcon(":icons/ok.png"),
+        QtGui.QApplication.translate("TrayIcon", "Check now...", None, QtGui.QApplication.UnicodeUTF8), menu)
+    actionConfigure = QtGui.QAction(QtGui.QIcon(":icons/configure.png"),
+        QtGui.QApplication.translate("TrayIcon", "Configure...", None, QtGui.QApplication.UnicodeUTF8), menu)
+    actionStatistics = QtGui.QAction(QtGui.QIcon(":icons/today.png"),
+        QtGui.QApplication.translate("TrayIcon", "Statistics...", None, QtGui.QApplication.UnicodeUTF8), menu)
+    actionQuit = QtGui.QAction(QtGui.QIcon(":icons/exit.png"),
+        QtGui.QApplication.translate("TrayIcon", "Exit", None, QtGui.QApplication.UnicodeUTF8), menu)
 
     menu.addAction(actionCheckQuota)
     menu.addAction(actionConfigure)
@@ -294,11 +294,11 @@ def main():
     menu.addSeparator()
     menu.addAction(actionQuit)
 
-    QObject.connect(actionQuit, SIGNAL("activated()"), app.quit)
-    QObject.connect(actionConfigure, SIGNAL("activated()"), configWindow.show)
-    QObject.connect(actionCheckQuota, SIGNAL("activated()"), trayIcon.checkQuota)
-    QObject.connect(actionStatistics, SIGNAL("activated()"), trayIcon.statsWindow.show)
-    QObject.connect(actionAbout, SIGNAL("activated()"), about)
+    QtCore.QObject.connect(actionQuit, QtCore.SIGNAL("activated()"), app.quit)
+    QtCore.QObject.connect(actionConfigure, QtCore.SIGNAL("activated()"), configWindow.show)
+    QtCore.QObject.connect(actionCheckQuota, QtCore.SIGNAL("activated()"), trayIcon.checkQuota)
+    QtCore.QObject.connect(actionStatistics, QtCore.SIGNAL("activated()"), trayIcon.statsWindow.show)
+    QtCore.QObject.connect(actionAbout, QtCore.SIGNAL("activated()"), about)
 
     trayIcon.setContextMenu(menu)
     trayIcon.show()
